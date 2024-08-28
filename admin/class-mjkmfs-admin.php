68a07acd4400c343d2060a1a695573911a20dc61
kmjamsheed0 <kmjamsheed0@gmail.com>
2024-08-28 19:11:18 +0530

    Daily Commit.

--- admin/class-mjkmfs-admin.php
+++ admin/class-mjkmfs-admin.php
@@ -17,7 +17,119 @@ if(!class_exists('MJKMFS_Admin')) :
 class MJKMFS_Admin {
 
     public function mjkmfs_write_panel_tab() {
-        // Code for adding the Force Sell panel in the product editor.
+        
+        global $post;
+        ?>
+        <p class="form-field">
+            <label for="force_sell_ids"><?php _e( 'Optional Add-ons', 'woo-force-sells' ); ?></label>
+            <?php
+                $product_ids = $this->mjkmfs_get_force_sell_ids( $post->ID, array( 'normal' ) );
+                $json_ids    = array();
+
+                if ( version_compare( WC_VERSION, '3.0', '>=' ) ) { ?>
+                    <select id="force_sell_ids" class="wc-product-search" multiple="multiple" style="width: 50%;" name="force_sell_ids[]" data-placeholder="<?php esc_attr_e( 'Search for a product&hellip;', 'woo-force-sells' ); ?>" data-action="woocommerce_json_search_products_and_variations" data-exclude="<?php echo intval( $post->ID ); ?>" data-exclude_type="variable">
+
+                    <?php
+                        foreach ( $product_ids as $product_id ) {
+                            $product = wc_get_product( $product_id );
+
+                            if ( ! $product ) {
+                                continue;
+                            }
+                    ?>
+                            <option value="<?php echo esc_attr( $product_id ); ?>" selected="selected"><?php echo wp_kses_post( $product->get_formatted_name() ); ?></option>
+                    <?php } ?>
+                    </select>
+            <?php } else { ?>
+                    <input type="hidden" class="wc-product-search" style="width: 50%;" id="force_sell_ids" name="force_sell_ids" data-placeholder="<?php _e( 'Search for a product&hellip;', 'woo-force-sells' ); ?>" data-action="woocommerce_json_search_products_and_variations" data-multiple="true" data-selected="<?php
+                    foreach ( $product_ids as $product_id ) {
+                        $product = wc_get_product( $product_id );
+
+                        if ( ! $product ) {
+                            continue;
+                        }
+
+                        $json_ids[ $product_id ] = wp_kses_post( $product->get_formatted_name() );
+                    }
+                    $json_ids_data = wp_json_encode( $json_ids );
+                    $json_ids_data = function_exists( 'wc_esc_json' ) ? wc_esc_json( $json_ids_data ) : _wp_specialchars( $json_ids_data, ENT_QUOTES, 'UTF-8', true );
+
+                    echo $json_ids_data;
+                    ?>" value="<?php echo implode( ',', array_keys( $json_ids ) ); ?>" />
+            <?php } ?>
+            <?php echo wc_help_tip( __( 'Add-on product can be removed or its quantity edited independently of the main product in the cart.', 'woo-force-sells' ) ); ?>
+        </p>
+        <p class="form-field">
+            <label for="force_sell_synced_ids"><?php _e( 'Mandatory Add-ons', 'woo-force-sells' ); ?></label>
+            <?php
+                $product_ids = $this->mjkmfs_get_force_sell_ids( $post->ID, array( 'synced' ) );
+                $json_ids    = array();
+
+                if ( version_compare( WC_VERSION, '3.0', '>=' ) ) { ?>
+                    <select id="force_sell_synced_ids" class="wc-product-search" multiple="multiple" style="width: 50%;" name="force_sell_synced_ids[]" data-placeholder="<?php esc_attr_e( 'Search for a product&hellip;', 'woo-force-sells' ); ?>" data-action="woocommerce_json_search_products_and_variations" data-exclude="<?php echo intval( $post->ID ); ?>" data-exclude_type="variable">
+
+                    <?php
+                        foreach ( $product_ids as $product_id ) {
+                            $product = wc_get_product( $product_id );
+
+                            if ( ! $product ) {
+                                continue;
+                            }
+                    ?>
+                            <option value="<?php echo esc_attr( $product_id ); ?>" selected="selected"><?php echo wp_kses_post( $product->get_formatted_name() ); ?></option>
+                    <?php } ?>
+                    </select>
+            <?php } else { ?>
+                <input type="hidden" class="wc-product-search" style="width: 50%;" id="force_sell_synced_ids" name="force_sell_synced_ids" data-placeholder="<?php _e( 'Search for a product&hellip;', 'woo-force-sells' ); ?>" data-action="woocommerce_json_search_products_and_variations" data-multiple="true" data-selected="<?php
+                foreach ( $product_ids as $product_id ) {
+                    $product = wc_get_product( $product_id );
+
+                    if ( ! $product ) {
+                        continue;
+                    }
+
+                    $json_ids[ $product_id ] = wp_kses_post( $product->get_formatted_name() );
+                }
+
+                $json_ids_data = wp_json_encode( $json_ids );
+                $json_ids_data = function_exists( 'wc_esc_json' ) ? wc_esc_json( $json_ids_data ) : _wp_specialchars( $json_ids_data, ENT_QUOTES, 'UTF-8', true );
+
+                echo $json_ids_data;
+                ?>" value="<?php echo implode( ',', array_keys( $json_ids ) ); ?>" />
+            <?php } ?>
+            <?php echo wc_help_tip( __( 'These products are forcefully added to the cart with the main product, and their quantity is synced with the main product.', 'woo-force-sells' ) ); ?>
+        </p>
+        <?php
+    }
+
+    /**
+     * Get force sell IDs from a given product ID and force sell type(s).
+     *
+     * @param int   $product_id Product ID.
+     * @param array $types      Force sell types (normal and/or synched).
+     *
+     * @return array Force sell IDs.
+     */
+    private function mjkmfs_get_force_sell_ids( $product_id, $types ) {
+        if ( ! is_array( $types ) || empty( $types ) ) {
+            return array();
+        }
+
+        $ids = array();
+
+        foreach ( $types as $type ) {
+            $new_ids = array();
+
+            if ( isset( $this->synced_types[ $type ] ) ) {
+                $new_ids = get_post_meta( $product_id, $this->synced_types[ $type ]['meta_name'], true );
+
+                if ( is_array( $new_ids ) && ! empty( $new_ids ) ) {
+                    $ids = array_merge( $ids, $new_ids );
+                }
+            }
+        }
+
+        return $ids;
     }
 
     public function mjkmfs_process_extra_product_meta( $post_id, $post ) {
